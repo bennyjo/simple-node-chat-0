25cdaf85aec67d5bea057aedcddcbaa12206c0ba
bennyjo <benny.json@gmail.com>
2013-10-11 14:12:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+var port = 3700;
+
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+
+app.get('/', function (req, res) {
+	res.render('page');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+	socket.emit('message', { message: 'Welcome to the chat!' });
+	socket.on('send', function (data) {
+		io.sockets.emit('message', data);
+	});
+});
+
+server.listen(3700);
+console.log('listening on port ' + port);
---
+++ public/chat.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+	var socket = io.connect('http://localhost:3700');
+	var field = document.querySelector('.field');
+	var name = document.querySelector('.name');
+	var sendButton = document.querySelector('.send');
+	var content = document.querySelector('#content');
+
+	// Show message in the content section
+	socket.on('message', function(data) {
+		var fragment = document.createDocumentFragment();
+
+		if (data.name) {
+			var pName = document.createElement('p');
+			pName.textContent = data.name;
+			pName.style.marginBottom = '0px';
+			fragment.appendChild(pName);
+		}
+
+		if (data.message) {
+			var pMessage = document.createElement('p');
+			pMessage.textContent = data.message;
+			pMessage.style.marginTop = '0px';
+			fragment.appendChild(pMessage);
+		}
+
+		content.appendChild(fragment);
+	});
+
+	// Send message when send button is clicked
+	sendButton.onclick = function() {
+		if(name.value) {
+			socket.emit('send', { name: name.value, message: field.value });
+			field.value = '';
+		} else {
+			alert('please fill in a name');
+		}
+	};
+};
